af93ff6f0b4063ba7d145f60a2deb8198bb55398
Yossi <yossi.kayhko@gmail.com>
2019-04-15 10:22:10 +0300

    added models

--- hotel/visitors/models.py
+++ hotel/visitors/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+	first_name = 
+	last_name =
+	phone_number = 
+	credit_card = 
+	country =
+
+
+
+
+class Booking(models.Model):
+	customer = 
+	room = 
+	date =
+
+
+class Room(models.Model):
+	room_type =
+	price =
+	

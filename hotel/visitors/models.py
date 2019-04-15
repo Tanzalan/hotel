13eb66deba52ca1a3bdc55d189adf6f37f22c9dc
Yossi <yossi.kayhko@gmail.com>
2019-04-15 10:52:51 +0300

    Fixed the models and Admin

--- hotel/visitors/models.py
+++ hotel/visitors/models.py
@@ -1,25 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
-
-
-class Customer(models.Model):
-	first_name = 
-	last_name =
-	phone_number = 
-	credit_card = 
-	country =
 
 
 
 
 class Booking(models.Model):
-	customer = 
-	room = 
-	date =
+	customer = models.ForeignKey(User, on_delete=models.CASCADE)
+	room = models.ForeignKey('Room', on_delete=models.CASCADE)
+	start_date = models.DateTimeField(null=True)
+	end_date = models.DateTimeField(null=True)
 
 
 class Room(models.Model):
-	room_type =
-	price =
-	
+	room_number = models.IntegerField(default=0)
+	price = models.IntegerField(default=0)
+
